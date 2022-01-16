56de2fdee2e2f158ead1c9856fd2dad9262ef25e
Robin van der Vliet <info@robinvandervliet.com>
2022-01-16 15:42:58 +0100

    Add script to generate CSV file based on JSON

---
+++ _admin/generate_csv.php
@@ -0,0 +1,68 @@
+<?php
+
+$keys = [
+	'dato',
+	'nomo',
+	'shildnomo',
+	'retposhtadreso',
+	'naskightago',
+	'loghlando',
+	'shildlando',
+	'membreco',
+	'ueakodo',
+	'alveno',
+	'foriro',
+	'loghado',
+	'kunloghado',
+	'kunloghanto',
+	'dormo',
+	'littuko',
+	'mangho',
+	'chemizo',
+	'donaco',
+	'pagmaniero',
+	'listo',
+	'fotoj',
+	'novulo',
+	'dieto',
+	'vizo',
+	'vizo-nacieco',
+	'vizo-pasportnumero',
+	'vizo-dato-ek',
+	'vizo-dato-ghis',
+	'vizo-adreso',
+	'volontulo',
+	'kontribuo',
+	'komentoj',
+];
+
+foreach (range(20, 27) as $day) {
+	for ($j = 0; $j < 3; $j++) {
+		if (
+			($day == 20 && $j != 2) ||
+			($day == 27 && $j != 0)
+		) { continue; }
+		$data[] = "mangho-$day-$j";
+	}
+}
+
+echo '#';
+foreach ($keys as $key) {
+	echo "\t" . $key;
+}
+echo "\n";
+
+$fh = fopen('../alighoj.csv', 'r');
+$i = 1;
+
+while (($line = fgets($fh)) !== false) {
+	$data = json_decode($line, true);
+
+	echo $i++;
+	foreach ($keys as $key) {
+		echo "\t" . ($data[$key] ?? '');
+	}
+	echo "\n";
+}
+
+fclose($fh);
